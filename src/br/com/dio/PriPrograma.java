90678af86686f0a6f2cccf96c79bb8b2b282600d
Newton Melo <new_business@outlook.com.br>
2022-01-03 11:42:43 -0300

    first commit

---
+++ src/br/com/dio/PriPrograma.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PriPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato Gato = new Gato();
+		Livros Livros = new Livros();
+		
+		System.out.println(Gato );
+		System.out.println(Livros);
+		
+		/*int a = 2022;
+		System.out.println("Hello World! " + a);*/
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag; 
+	
+}
